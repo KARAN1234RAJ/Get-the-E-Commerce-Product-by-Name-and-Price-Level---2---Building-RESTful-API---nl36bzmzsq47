d1be225b0926db53891be88ed300b49cdc858efb
KARAN1234RAJ <36782443+KARAN1234RAJ@users.noreply.github.com>
2024-03-26 18:03:50 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,5 +10,19 @@ app.use(express.json());
 
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
+app.get("/api/v1/products/:id", (req, res) => {
+  const productId = req.params.id;
+  // console.log(productId);
+  let singleProduct = products.find((item) => item.id == productId);
+  if (singleProduct) {
+    res
+      .status(200)
+      .send({ status: "success", message: "Product found", data: singleProduct });
+  } else {
+    res
+      .status(404)
+      .send({ status: "failed", message: "Product not found" });
+  }
+});
 
 module.exports = app;
